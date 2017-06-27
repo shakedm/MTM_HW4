49b0c3d577d831da6bfcabb21b3b7720d681b2df
AdiSharon <adi.sharon@campus.technion.ac.il>
2017-06-27 14:42:21 +0300

    Company2

--- Company.cpp
+++ Company.cpp
@@ -7,46 +7,46 @@ namespace mtm{
 
         Company::Company(const Company &company) {}
 
-        Company& Company::operator=(const Company &company) {}
+        Company& Company::operator=(const Company &company) {} //Adi
 
         void Company::createRoom(char *name, const int &escapeTime,
                                  const int &level,
-                                 const int &maxParticipants) {}
+                                 const int &maxParticipants) {} //Adi
 
         void Company::createScaryRoom(char *name, const int &escapeTime,
                                       const int &level,
                                       const int &maxParticipants,
                                       const int &ageLimit,
-                                      const int &numOfScaryEnigmas) {}
+                                      const int &numOfScaryEnigmas) {} //Shaked
 
         void Company::createKidsRoom(char *name, const int &escapeTime,
                                      const int &level,
                                      const int &maxParticipants,
-                                     const int &ageLimit) {}
+                                     const int &ageLimit) {} //Shaked
 
-        set<EscapeRoomWrapper*> Company::getAllRooms() const {}
+        set<EscapeRoomWrapper*> Company::getAllRooms() const {} //Adi
 
-        void Company::removeRoom(const EscapeRoomWrapper &room) {}
+        void Company::removeRoom(const EscapeRoomWrapper &room) {} //Adi
 
         void Company::addEnigma(const EscapeRoomWrapper &room,
-                                const Enigma &enigma) {}
+                                const Enigma &enigma) {} //Shaked
 
         void Company::removeEnigma(const EscapeRoomWrapper &room,
-                                   const Enigma &enigma) {}
+                                   const Enigma &enigma) {} //Shaked
 
         void Company::addItem(const EscapeRoomWrapper &room,
-                              const Enigma &enigma, const string &element) {}
+                              const Enigma &enigma, const string &element) {} //Adi
 
         void Company::removeItem(const EscapeRoomWrapper &room,
-                                 const Enigma &enigma, const string &element) {}
+                                 const Enigma &enigma, const string &element) {} //Adi
 
         set<EscapeRoomWrapper*> Company::getAllRoomsByType(
-                RoomType type) const {}
+                RoomType type) const {} //Shaked
 
-        EscapeRoomWrapper Company::getRoomByName(const string &name) const {}
+        EscapeRoomWrapper Company::getRoomByName(const string &name) const {} //Adi
 
         std::ostream& Company::operator<<(std::ostream &output,
-                                          const Company &company) {}
+                                          const Company &company) {} //Shaked
 
         Company::~Company() {}
     }
